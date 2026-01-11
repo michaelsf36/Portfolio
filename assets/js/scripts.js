69f8f3754150ee628163c9292591610b2b7601f8
Michael <michael.sf36@yahoo.com.br>
2026-01-11 17:48:24 -0300

    chore: create scripts folder in assets

---
+++ assets/js/scripts.js
@@ -0,0 +1,48 @@
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const formulario = document.querySelector('#formulario');
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const campoNome = document.querySelector('#name');
+    const errNome = document.querySelector('#errNome');
+
+    if (campoNome.value.length < 3) {
+        errNome.innerHTML = 'O nome deve ter no minimo 3 caracteres.';
+        campoNome.focus();
+        return;
+    }else{
+        errNome.innerHTML = '';
+    }
+
+    const campoEmail = document.querySelector('#email');
+    const errEmail = document.querySelector('#errEmail');
+
+    if (!campoEmail.value.match(emailRegex)) {
+        errEmail.innerHTML = 'Digite um email válido.';
+        campoEmail.focus();
+        return;
+    }else{
+        errEmail.innerHTML = '';
+    }
+
+    const campoSubject = document.querySelector('#subject');
+    const errSubject = document.querySelector('#errSubject');
+
+    if (campoSubject.value.length < 5) {
+        errSubject.innerHTML = 'o assunto deve ter no minimo 5 caracteres.';
+        campoSubject.focus();
+        return;
+    }else{
+        errSubject.innerHTML = '';
+    }
+
+    const campoMessage = document.querySelector('#message');
+
+    window.alert("Formulario enviado com sucesso!");
+
+    campoNome.value = ''
+    campoEmail.value =  ''
+    campoSubject.value = ''
+    campoMessage.value = ''
+});
